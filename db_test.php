35a3c8e6db91a969a221a1730d1a719f1bcc31ad
gitbouzu <jtadanobouzu@yahoo.co.jp>
2014-08-17 20:53:24 +0900

    Add: mysqli_close() function

--- db_test.php
+++ db_test.php
@@ -1,8 +1,10 @@
-<!--?php <br ?--> $link = mysqli_connect('localhost', 'root', '');
+<!--?php 
+$link = mysqli_connect('localhost', 'root', '');
 if (!$link) {
 die('接続失敗！'.mysqli_error());
 } else {
 echo '接続成功！';
 mysqli_set_charset($link, "utf8")
+mysqli_close($link);
 }
-?&gt;
+?>
